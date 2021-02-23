f8cf41a698791ba0dabb66d50a91a3b6ac9d5a59
Bruno Mendes <up201906166@fe.up.pt>
2021-02-23 22:23:52 +0000

    Encapsulate tangram

--- tp2/MyScene.js
+++ tp2/MyScene.js
@@ -4,6 +4,7 @@ import { MyTriangle } from "./MyTriangle.js";
 import { MyParallelogram } from "./MyParallelogram.js";
 import { MyTriangleSmall } from "./MyTriangleSmall.js";
 import { MyTriangleBig } from "./MyTriangleBig.js";
+import { MyTangram } from "./MyTangram.js";
 
 /**
  * MyScene
@@ -29,22 +30,12 @@ export class MyScene extends CGFscene {
 
     //Initialize scene objects
     this.axis = new CGFaxis(this);
-    this.greenTriangle = new MyDiamond(this);
-    this.pinkTriangle = new MyTriangle(this);
-    this.yellowParallelogram = new MyParallelogram(this);
-    this.redTriangle = new MyTriangleSmall(this);
-    this.purpleTriangle = new MyTriangleSmall(this);
-    this.orangeTriangle = new MyTriangleBig(this);
-    this.blueTriangle = new MyTriangleBig(this);
+    this.tangram = new MyTangram(this);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
     this.scaleFactor = 1;
-    this.displayDiamond = false;
-    this.displayTriangle = false;
-    this.displayParallelogram = false;
-    this.displayTriangleSmall = false;
-    this.displayTriangleBig = false;
+    this.displayTangram = false;
   }
   initLights() {
     this.lights[0].setPosition(15, 2, 5, 1);
@@ -106,169 +97,8 @@ export class MyScene extends CGFscene {
 
     // ---- BEGIN Primitive drawing section
 
-    /* Green square */
-    let translateMDiamond =
-      [1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        0.5, 0.5, 0, 1];
-    let DiamondRotateAngle = Math.PI / 4;
-    let rotateMDiamond =
-      [Math.cos(DiamondRotateAngle), Math.sin(DiamondRotateAngle), 0, 0,
-      -Math.sin(DiamondRotateAngle), Math.cos(DiamondRotateAngle), 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1];
-    let scaleGeneral =
-      [1 / Math.sqrt(2), 0, 0, 0,
-        0, 1 / Math.sqrt(2), 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1];
-
-    this.pushMatrix();
-    this.multMatrix(translateMDiamond);
-    this.multMatrix(rotateMDiamond);
-    this.multMatrix(scaleGeneral);
-    if (this.displayDiamond) this.greenTriangle.display();
-    this.popMatrix();
-
-    /* Red triangle */
-    let translateSTriangle =
-      [1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        0.5, 0.5 + 1, 0, 1];
-    let rotateSTriangle1 =
-      [Math.cos(-DiamondRotateAngle * 3), Math.sin(-DiamondRotateAngle * 3), 0, 0,
-      -Math.sin(-DiamondRotateAngle * 3), Math.cos(-DiamondRotateAngle * 3), 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1];
-    this.pushMatrix();
-    this.multMatrix(translateSTriangle);
-    this.multMatrix(rotateSTriangle1);
-    this.multMatrix(scaleGeneral);
-    if (this.displayTriangleSmall) this.redTriangle.display();
-    this.popMatrix();
-
-    /* Orange triangle */
-    let orangeTriangleRotateAngle = Math.PI / 4;
-    let rotateOrangeTriangle =
-      [Math.cos(orangeTriangleRotateAngle), Math.sin(orangeTriangleRotateAngle), 0, 0,
-      -Math.sin(orangeTriangleRotateAngle), Math.cos(orangeTriangleRotateAngle), 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1];
-    let translateOrangeTriangle =
-      [1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        1, 2, 0, 1];
-    this.pushMatrix();
-    this.multMatrix(translateOrangeTriangle);
-    this.multMatrix(rotateOrangeTriangle);
-    this.multMatrix(scaleGeneral);
-    if (this.displayTriangleBig) this.orangeTriangle.display();
-    this.popMatrix();
-
-    /* Pink triangle */
-    let pinkTriangleRotateAngle = 5 * Math.PI / 4;
-    let pinkRotateTriangle =
-      [Math.cos(pinkTriangleRotateAngle), Math.sin(pinkTriangleRotateAngle), 0, 0,
-      -Math.sin(pinkTriangleRotateAngle), Math.cos(pinkTriangleRotateAngle), 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1];
-    let translatePinkTriangle =
-      [1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        2, 3, 0, 1];
-    this.pushMatrix();
-    this.multMatrix(translatePinkTriangle);
-    this.multMatrix(pinkRotateTriangle);
-    this.multMatrix(scaleGeneral);
-    if (1 || this.displayTriangle) this.pinkTriangle.display();
-    this.popMatrix();
-
-    /* Purple triangle */
-
-    let purpleTriangleRotateAngle = 3 * Math.PI / 4;
-    let rotatePurpleTriangle =
-      [Math.cos(purpleTriangleRotateAngle), Math.sin(purpleTriangleRotateAngle), 0, 0,
-      -Math.sin(purpleTriangleRotateAngle), Math.cos(purpleTriangleRotateAngle), 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1];
-
-    let translatePurpleTriangle =
-      [1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        3.5, 0.5, 0, 1];
-    this.pushMatrix()
-
-    this.multMatrix(translatePurpleTriangle);
-    this.multMatrix(rotatePurpleTriangle);
-    this.multMatrix(scaleGeneral);
-
-    this.purpleTriangle.display();
-
-    this.popMatrix();
-
-
-    /* Yellow paralelogram */
-
-    let yellowParallelogramRotateAngleZ = -3 * Math.PI / 4;
-    let rotateyellowParallelogramZ =
-      [Math.cos(yellowParallelogramRotateAngleZ), Math.sin(yellowParallelogramRotateAngleZ), 0, 0,
-      -Math.sin(yellowParallelogramRotateAngleZ), Math.cos(yellowParallelogramRotateAngleZ), 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1];
-
-    let yellowParallelogramRotateAngleX = Math.PI;
-    let rotateyellowParallelogramX =
-      [1, 0, 0, 0,
-        0, Math.cos(yellowParallelogramRotateAngleX), Math.sin(yellowParallelogramRotateAngleX), 0,
-        0, -Math.sin(yellowParallelogramRotateAngleX), Math.cos(yellowParallelogramRotateAngleX), 0,
-        0, 0, 0, 1];
-
-    let translateYellowParallelogram =
-      [1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        4, 0, 0, 1];
-
-    this.pushMatrix()
-
-    this.multMatrix(translateYellowParallelogram);
-    this.multMatrix(rotateyellowParallelogramX);
-    this.multMatrix(rotateyellowParallelogramZ);
-    this.multMatrix(scaleGeneral);
-    this.yellowParallelogram.display();
-
-    this.popMatrix();
-
-    /* Blue Triangle */
-
-    let blueTriangleRotateAngle = -Math.PI / 4;
-    let rotateBlueTriangle =
-      [Math.cos(blueTriangleRotateAngle), Math.sin(blueTriangleRotateAngle), 0, 0,
-      -Math.sin(blueTriangleRotateAngle), Math.cos(blueTriangleRotateAngle), 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1];
-
-    let translateBlueTriangle =
-      [1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        3, 2, 0, 1];
-
-    this.pushMatrix()
-
-    this.multMatrix(translateBlueTriangle);
-    this.multMatrix(rotateBlueTriangle);
-    this.multMatrix(scaleGeneral);
-    this.blueTriangle.display();
-
-    this.popMatrix();
+    if (this.displayTangram) this.tangram.display();
 
     // ---- END Primitive drawing section
   }
-
 }
