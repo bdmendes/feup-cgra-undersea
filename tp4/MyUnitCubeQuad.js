58f1468cb8dca0e9bd740266128d5bc4a1ea273a
Francisco Colino <up201905405@fe.up.pt>
2021-03-09 17:35:21 +0000

    MyUnitCubeQuad textures now have the correct orientation

--- tp4/MyUnitCubeQuad.js
+++ tp4/MyUnitCubeQuad.js
@@ -48,18 +48,21 @@ export class MyUnitCubeQuad {
                 this.scene.pushMatrix();
                 this.scene.rotate(-Math.PI/2, 1, 0, 0);
                 this.scene.translate(0, 0, 0.5);
+                this.scene.rotate(Math.PI, 0, 0, 1);
                 this.quad.display();
                 this.scene.popMatrix();
 
                 this.scene.pushMatrix();
                 this.scene.rotate(Math.PI/2, 0, 1, 0);
                 this.scene.translate(0, 0, 0.5);
+                this.scene.rotate(Math.PI/2, 0, 0, 1);
                 this.quad.display();
                 this.scene.popMatrix();
 
                 this.scene.pushMatrix();
                 this.scene.rotate(-Math.PI/2, 0, 1, 0);
                 this.scene.translate(0, 0, 0.5);
+                this.scene.rotate(-Math.PI/2, 0, 0, 1);
                 this.quad.display();
                 this.scene.popMatrix();
         }
